babff65fafe51058cdb5064f4e8354d8dfdd67ac
Diego Nieto Cid <dnietoc@gmail.com>
2015-03-20 16:29:47 -0300

    Don't build xulrunner, use Mozilla's sdk

--- .travis/xulrunner/build.sh
+++ .travis/xulrunner/build.sh
@@ -7,7 +7,7 @@ trap 'STATUS=$?; cd "$CWD"; exit $STATUS' ERR
 
 WORKING_DIR="`cd .. && pwd`/chmsee-deps/xulrunner"
 VERSION=31.0
-URL="ftp://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/$VERSION/source/xulrunner-$VERSION.source.tar.bz2"
+URL="ftp://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/$VERSION/sdk/xulrunner-$VERSION.en_US.linux-x86_64.sdk.tar.bz2"
 
 sudo apt-get install libgtk2.0-dev libnss3-dev libxt-dev libhunspell-dev libstartup-notification0-dev libdbus-glib-1-dev libpulse-dev libevent-dev libvpx-dev libicu-dev libffi-dev libasound2-dev libicu-dev zip unzip pkg-config diffutils yasm libegl1-mesa-dev libgstreamer-plugins-base0.10-dev wget
 
@@ -16,47 +16,6 @@ cd "$WORKING_DIR"
 
 wget -c "$URL"
 
-mkdir src
-tar -xjf "xulrunner-$VERSION.source.tar.bz2" -C src
-
-cd src/mozilla-release
-cp "$CWD/.travis/xulrunner/mozconfig" .mozconfig
-
-heartbeat() {
-  local i=0
-  while true; do
-    echo -n "."
-    let "i += 1"
-    if [ "$i" -eq 79 ]; then
-      echo
-      let i=0 || true
-    fi
-    sleep 1
-  done
-}
-heartbeat &
-HEARTBEAT_PID=$!
-
-build() {
-     make -f client.mk build \
-  || make -f client.mk build \
-  || make -f client.mk build
-}
-
-build 2>&1 | gzip -c > "$WORKING_DIR/build.log.gz" || {
-	STATUS=$?
-	kill $HEARTBEAT_PID || true
-	echo
-	echo Recent log:
-	zcat "$WORKING_DIR/build.log.gz" | tail -n500
-	exit $STATUS
-}
-
-kill $HEARTBEAT_PID || true
-echo Recent log:
-zcat "$WORKING_DIR/build.log.gz" | tail -n500
-
-
-sudo make -f client.mk DESTDIR="/usr" install
+tar -xjf "xulrunner-$VERSION.source.tar.bz2"
 
 cd "$CWD"
