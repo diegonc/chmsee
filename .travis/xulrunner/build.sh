256e1c1d038b406102300d90260d70ce3c411ae7
Diego Nieto Cid <dnietoc@gmail.com>
2015-03-20 17:02:22 -0300

    Fix tar file name

--- .travis/xulrunner/build.sh
+++ .travis/xulrunner/build.sh
@@ -7,7 +7,8 @@ trap 'STATUS=$?; cd "$CWD"; exit $STATUS' ERR
 
 WORKING_DIR="`cd .. && pwd`/chmsee-deps/xulrunner"
 VERSION=31.0
-URL="ftp://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/$VERSION/sdk/xulrunner-$VERSION.en-US.linux-x86_64.sdk.tar.bz2"
+TAR_FILE=xulrunner-$VERSION.en-US.linux-x86_64.sdk.tar.bz2
+URL="ftp://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/$VERSION/sdk/$TAR_FILE"
 
 sudo apt-get install libgtk2.0-dev libnss3-dev libxt-dev libhunspell-dev libstartup-notification0-dev libdbus-glib-1-dev libpulse-dev libevent-dev libvpx-dev libicu-dev libffi-dev libasound2-dev libicu-dev zip unzip pkg-config diffutils yasm libegl1-mesa-dev libgstreamer-plugins-base0.10-dev wget
 
@@ -16,6 +17,6 @@ cd "$WORKING_DIR"
 
 wget -c "$URL"
 
-tar -xjf "xulrunner-$VERSION.source.tar.bz2"
+tar -xjf "$TAR_FILE"
 
 cd "$CWD"
