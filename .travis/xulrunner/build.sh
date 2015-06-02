6d0114710eec968e997ac6dfa5120acd1d5f6760
Diego Nieto Cid <dnietoc@gmail.com>
2015-06-01 21:22:19 -0300

    Try to fix update-alternatives call.

--- .travis/xulrunner/build.sh
+++ .travis/xulrunner/build.sh
@@ -10,10 +10,12 @@ VERSION=38.0.1
 TAR_FILE=xulrunner-$VERSION.en-US.linux-x86_64.sdk.tar.bz2
 URL="ftp://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/$VERSION/sdk/$TAR_FILE"
 
+sudo update-alternatives --query gcc || true
+sudo update-alternatives --query g++ || true
+
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get install -qq g++-4.8
-sudo update-alternatives --set g++ /usr/bin/g++-4.8
 sudo update-alternatives --set gcc /usr/bin/gcc-4.8
 
 mkdir -p "$WORKING_DIR"
