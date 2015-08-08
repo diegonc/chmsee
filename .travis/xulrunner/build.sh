087fccc6bd2da4b9640c8031c553048639a99991
Diego Nieto Cid <dnietoc@gmail.com>
2015-08-08 00:39:44 -0300

    Update xulrunner URL

--- .travis/xulrunner/build.sh
+++ .travis/xulrunner/build.sh
@@ -8,7 +8,7 @@ trap 'STATUS=$?; cd "$CWD"; exit $STATUS' ERR
 WORKING_DIR="`cd .. && pwd`/chmsee-deps/xulrunner"
 VERSION=39.0
 TAR_FILE=xulrunner-$VERSION.en-US.linux-x86_64.sdk.tar.bz2
-URL="ftp://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/$VERSION/sdk/$TAR_FILE"
+URL="http://archive.mozilla.org/pub/xulrunner/releases/$VERSION/sdk/$TAR_FILE"
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
